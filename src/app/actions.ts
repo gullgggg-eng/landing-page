81fc8e2abfa4ad1c1c368e0fe5893a0ef4bad917
Eugene <eugene@gmail.com>
2026-01-05 21:39:31 +0200

    fix: use FormData_5

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -4,7 +4,7 @@ export async function sendEmail(formData: FormData) {
   const data = Object.fromEntries(formData);
   data.access_key = "063b7fa1-296a-497a-b9c2-6d9259464e42";
 
-  const response = await fetch("https://api.web3forms.com", {
+  const response = await fetch("https://api.web3forms.com/submit", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
